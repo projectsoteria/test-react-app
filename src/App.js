4025b6c0800904f17037983c73c29b6e8dad8449
Shreya Ravi <projectsoteria2020@gmail.com>
2020-06-20 14:26:57 -0400

    changed background color, added bread picture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from 'react-bootstrap/Image'
+
 
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+      <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Korb_mit_Br%C3%B6tchen.JPG/500px-Korb_mit_Br%C3%B6tchen.JPG" fluid />
+      </header>
     </div>
   );
 }
