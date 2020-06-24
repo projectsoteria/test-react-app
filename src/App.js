d88de2a73b5e2d285a9c690c875646c7db31230b
Shreya Ravi <projectsoteria2020@gmail.com>
2020-06-24 10:43:01 -0400

    add image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
 import Image from 'react-bootstrap/Image'
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Korb_mit_Br%C3%B6tchen.JPG/500px-Korb_mit_Br%C3%B6tchen.JPG" fluid />
+        <Card>
+        <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Korb_mit_Br%C3%B6tchen.JPG/500px-Korb_mit_Br%C3%B6tchen.JPG" height="680" width= "790"/>
+          <Card.Body background-color= "#FFB6C1">
+            <Card.Text>
+              This article is about bread, and everything that bread is a fan of. How bread originated, where bread came from, who bread is. Bread!
+            </Card.Text>
+            <Button variant="outline-info" src="google.com">Info</Button>{' '}
+          </Card.Body>
+        </Card>
       </header>
     </div>
   );
