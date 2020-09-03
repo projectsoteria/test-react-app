e8730bd09a6ebe0cb9d8762084cb829816d99c65
Shreya Ravi <projectsoteria2020@gmail.com>
2020-09-03 17:30:27 -0400

    set up auth

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
